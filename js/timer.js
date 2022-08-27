13ab866ebd172a3b55ba5c81d956e59bfc5e57b5
JavohirKahhorov <javohirqahhorov2006@gmail.com>
2022-08-27 13:11:47 +0500

    .

--- js/timer.js
+++ js/timer.js
@@ -47,3 +47,22 @@ function addZero(num){
     }
 }
 changeTime(discount, '.discount__timer')
+
+// Timer 2
+// let discount = '2022-09-01'
+
+// function setInterval(discount){
+//     let time = Date.parse(discount) - Date.parse(new Date())
+//     let days = Math.floor(time / (24 * 60 * 60 * 1000))
+//     let hours = Math.floor(time / (60 * 60 * 1000) % 24)
+//     let minutes = Math.floor(time / (60 * 1000) % 60)
+//     let seconds = Math.floor(time / 1000)
+//     return ({
+//         'time': time,
+//         'time': time,
+//         'time': time,
+//         'time': time,
+//         'time': time,
+//     })
+// }
+// console.log(setInterval(discount))
